41cec1a1df56ad46a82dd3b965e6eba62fc2c702
Akshay Dsai <87740052+Akshayd079@users.noreply.github.com>
2024-07-22 22:58:22 +0800

    finish project

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,42 @@
+import styles from "./ProjectsStyles.module.css";
+import viberr from "../../assets/viberr.png";
+import freshBurger from "../../assets/fresh-burger.png";
+import hipsster from "../../assets/hipsster.png";
+import fitLift from "../../assets/fitlift.png";
+import ProjectCard from "../../common/Projectcard";
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/Akshayd079/JavaScript/tree/main/Expense%20tracker"
+          h3="Viberr"
+          p="Expense Tracker"
+        />
+        <ProjectCard
+          src={freshBurger}
+          link="https://github.com/Akshayd079/react_restaurant"
+          h3="Restaurant"
+          p="Indian Retuarant"
+        />
+        <ProjectCard
+          src={hipsster}
+          link="https://github.com/Akshayd079/JavaScript/tree/main/to%20do%20list"
+          h3="To Do"
+          p="To do list "
+        />
+        <ProjectCard
+          src={fitLift}
+          link="https://github.com/geeyas222/startup"
+          h3="SoftHat"
+          p="Web Solution"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
