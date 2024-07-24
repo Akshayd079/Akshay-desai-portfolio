89b45444406650889e44f55ff7f26c5c8a322505
Akshay Dsai <87740052+Akshayd079@users.noreply.github.com>
2024-07-24 16:18:10 +0800

    social media link

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -45,10 +45,10 @@ function Hero() {
           <a href="https://twitter.com/" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
           </a>
-          <a href="https://github.com/" target="_blank">
+          <a href="https://github.com/Akshayd079" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://linkedin.com/" target="_blank">
+          <a href="https://www.linkedin.com/in/akshaydesai079/" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
